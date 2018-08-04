d9b80b4293fc97fc0a9129ae9377f4865d53a6e7
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2018-08-04 11:21:54 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component'
+
+@NgModule({
+  imports:[
+    RouterModule.forRoot([
+           {path: '', component: HomeComponent}
+    ])  
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule{}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';   
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'as';
+  title = 'app';
 }
